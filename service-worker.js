32feb0b2d1ef2cef29686a050b71ae62419b5861
ics-watanabe <maya.watanabe@ics-web.jp>
2016-05-13 19:42:37 +0900

    リンク先URLが誤っていたため修正

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@ self.addEventListener('push', function(event) {
   var body = 'ServiceWorkerの記事を公開しました';
   var icon = 'ics_logo_512x512.png';
   var tag = 'simple-push-demo-notification-tag';
-  var url = 'https://ics.media//entry/11763';
+  var url = 'https://ics.media/entry/11763';
 
   event.waitUntil(
     self.registration.showNotification(title, {
