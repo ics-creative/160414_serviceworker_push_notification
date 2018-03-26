d788e40694f4ea4b5aa553cb18a761ce7d9dcb1f
ics-ikeda <ikeda@ics-web.jp>
2018-03-26 15:31:49 +0900

    ServiceWorker側もES2017に修正

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,21 @@
-'use strict';
-
-self.addEventListener('push', function (event) {
+// プッシュ通知が入ってきたときの処理
+self.addEventListener('push', (event) => {
   console.log('Received a push message', event);
 
   // サンプルでは固定のメッセージを通知するようにしています。
   // 動的にユーザーごとにメッセージを変えたい場合は、
   // ペイロードの暗号化を行うか、FetchAPIで動的に情報を取得する必要があります。
-  var title = '新着記事のお知らせです';
-  var body = 'ServiceWorkerの記事を公開しました';
-  var icon = 'ics_logo_512x512.png';
-  var tag = 'simple-push-demo-notification-tag';
-  var url = 'https://ics.media/entry/11763';
+  const title = '新着記事のお知らせです';
+  const body = 'ServiceWorkerの記事を公開しました';
+  const icon = 'ics_logo_512x512.png';
+  const tag = 'simple-push-demo-notification-tag';
+  const url = 'https://ics.media/entry/11763';
 
   event.waitUntil(
     self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: tag,
+      body,
+      icon,
+      tag,
       data: {
         url: url
       }
@@ -24,26 +23,27 @@ self.addEventListener('push', function (event) {
   );
 });
 
-self.addEventListener('notificationclick', function (event) {
+// 通知領域がクリックされたときの処理
+self.addEventListener('notificationclick', async (event) => {
   console.log('On notification click: ', event.notification.tag);
   event.notification.close();
 
-  var notoficationURL = '/';
+  let notoficationURL = '/';
   if (event.notification.data.url) {
     notoficationURL = event.notification.data.url;
   }
 
-  event.waitUntil(clients.matchAll({
+  const clientList = await event.waitUntil(clients.matchAll({
     type: 'window'
-  }).then(function (clientList) {
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url === '/' && 'focus' in client) {
-        return client.focus();
-      }
-    }
-    if (clients.openWindow) {
-      return clients.openWindow(notoficationURL);
-    }
   }));
+
+  for (let i = 0; i < clientList.length; i++) {
+    const client = clientList[i];
+    if (client.url === '/' && 'focus' in client) {
+      return client.focus();
+    }
+  }
+  if (clients.openWindow) {
+    return clients.openWindow(notoficationURL);
+  }
 });
